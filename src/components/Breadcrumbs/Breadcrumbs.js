7d493b94017d512bc82bc7258522fbb9348fe60a
Acolapsys <timur_sh@bk.ru>
2020-12-01 17:40:08 +0500

    fix: fix breadcrumbs single product title

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -1,29 +1,30 @@
 import React from "react";
-import { Link, useLocation, useParams, useRouteMatch } from "react-router-dom";
+import { Link, useLocation} from "react-router-dom";
 
 const titleList = {
-  "/cart": "Cart",
-  "/product": "Product page",
-  "/products": "Products results",
-  "/blog": "Blog",
-  "/account": "Account"
+  "cart": "Cart",
+  "product": "Product page",
+  "products": "Products results",
+  "blog": "Blog",
+  "account": "Account"
   
 };
 
 const Breadcrumbs = () => {
 
   const location = useLocation();
-  const curRoute = location.pathname.match(/^\/\w+/)
-  console.log( location, "curr=", curRoute);
+  const curRoute = location.pathname.split("/")[1]
+  
+  console.log( location, "curr=", location.pathname.split("/")[1]);
 
-  return ( location.pathnme !== '/' &&
+  return ( location.pathname !== '/' &&
     <div id="breadcrumbs">
       <div className="container">
         <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>{titleList[location.pathname]}</li>
+          <li>{titleList[curRoute]}</li>
         </ul>
       </div>
     </div>
